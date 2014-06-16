a099be9818d68cdc8edebb51dfb5bba8d871a017
Sharma <Kunal Sharma>
2014-06-16 10:31:16 -0700

    Problem 5

--- 5/5.ml
+++ 5/5.ml
@@ -1,3 +1,31 @@
+let rec gcd x y = 
+	if (y == 0) then 
+		x
+	else
+		gcd y (x mod y)
+	in
 
-let lcm l_in = 
-	let rec lcm_helper curr = 
+let lcm x y = (x*y)/(gcd x y) in
+
+let rec lcm_list l_in = 
+	match  l_in with 
+		[] -> 1 |
+		h::[] -> h 	|
+		h1::h2::[] -> (lcm h1 h2) |
+		h1::t -> (lcm h1 (lcm_list t))
+in
+
+let range x y = 
+	let rec range_helper curr ret = 
+		if (curr == x) then 
+			curr::ret
+		else
+			range_helper (curr - 1) (curr::ret)
+	in
+	range_helper y []
+in
+
+let output_int = lcm_list (range 1 20) in 
+
+print_int output_int;;
+print_char('\n');;
