56aa301c064836b71382041e54e573ac5661bd24
kunals1994 <Kunal Sharma>
2014-06-16 15:53:33 -0700

    Problem 6

---
+++ 6/6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define LOWER_BOUND 1
+#define UPPER_BOUND 100
+
+int main (int argc, char** argv){
+	int curr;
+	int sum_of_squares;
+	int square_of_sum; 
+
+	int diff;
+
+	sum_of_squares = square_of_sum = 0;
+
+	for (curr = LOWER_BOUND; curr <= UPPER_BOUND; curr ++){
+		sum_of_squares += curr * curr;
+		square_of_sum += curr;
+	}
+
+	square_of_sum *= square_of_sum;
+
+	diff = square_of_sum - sum_of_squares; 
+
+	printf("%d - %d = %d \n", square_of_sum, sum_of_squares, diff);
+
+}
