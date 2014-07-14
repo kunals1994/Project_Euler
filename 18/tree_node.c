ae6a2557a6b988fb97a97dbac4394bcd84bf80d1
kunals1994 <ksharma8@terpmail.umd.edu>
2014-07-14 16:48:59 -0700

    solved 18

--- 18/tree_node.c
+++ 18/tree_node.c
@@ -3,16 +3,7 @@
 #include <string.h>
 #include "tree_node.h"
 
-#define TRUE 0
-#define FALSE 1
-
 #define TREE_NODE_BUF_SIZE 1024
-
-struct tree_node{
-	int value;
-	struct tree_node* left;
-	struct tree_node* right;
-};
 
 struct tree_node* create_node(int val_in){
 	struct tree_node* ret = calloc(1, sizeof(struct tree_node));
@@ -25,18 +16,58 @@ struct tree_node* create_node(int val_in){
 struct tree_node* tree_from_file(char* file_path){
 	FILE* f_in = fopen(file_path, "r");
 	char* line = calloc(TREE_NODE_BUF_SIZE, sizeof(char));
-	struct tree_node* ret = calloc (1, sizeof(struct tree_node));
+	int curr_len;	
+	
+	struct tree_node** curr_row;
+	struct tree_node** prev_row;
+	int prev_row_len;
+	
+	struct tree_node* ret;
 
 	fgets(line, TREE_NODE_BUF_SIZE, f_in);
-	
+
+	ret = create_node(atoi(line));
+
+	prev_row  = calloc(1, sizeof(struct tree_node*));
+	prev_row[0] = ret;
+	prev_row_len = 1;
+
+	curr_len = 2;
 	while(fgets(line, TREE_NODE_BUF_SIZE, f_in) != NULL){
-		printf("%lu \n", strlen(line)/3);
+		curr_row = calloc(strlen(line)/3, sizeof(struct tree_node*));
+
+		char* curr_item = calloc(3, sizeof(char));
+
+		for (int curr = 0; curr < curr_len; curr++){
+			
+
+			strncpy(curr_item, line+(3*curr), 2);
+
+			curr_row[curr] = create_node(atoi(curr_item));
+
+			if(curr < prev_row_len){
+				prev_row[curr] -> left = curr_row[curr];
+			}
+
+			if(curr != 0){
+				prev_row[curr - 1] -> right = curr_row[curr];
+			}
+		}
+
+		free(prev_row);
+		prev_row = curr_row;
+		prev_row_len = curr_len;
+
+		free(curr_item);
+
+		curr_len +=1;
 	}
 
 	free(line);
+	free(curr_row);
 	fclose(f_in);
 
-	return NULL;
+	return ret;
 }
 
 int maximal_sum (struct tree_node* node_in){
@@ -44,34 +75,36 @@ int maximal_sum (struct tree_node* node_in){
 	
 	if(node_in == NULL){
 		return ret;
-	}else if(((node_in -> left) -> value) > ((node_in -> right) -> value)){
-		ret = node_in -> value + maximal_sum(node_in -> left);
+	}else if(node_in -> left == NULL && node_in -> right == NULL){
+		return node_in -> value;
 	}else{
-		ret = node_in -> value + maximal_sum(node_in -> right);
+		int right = maximal_sum(node_in -> right);
+		int left = maximal_sum(node_in -> left);
+
+		ret = (left > right) ? left : right;
+
+		ret += node_in -> value;
 	}
 	return ret;
+}
+
+static void destroy_left_node(struct tree_node* node_in){
+	if(node_in == NULL){return;}
+
+	destroy_left_node(node_in -> left);
+
+	free(node_in);
 }
 
 void destroy_tree (struct tree_node* node_in){
 	
 	if(node_in == NULL){return;}
 
-	struct tree_node* n1 = node_in -> left;
-	struct tree_node* n2 = node_in -> right;
+	destroy_left_node(node_in -> left);
+
+	destroy_tree(node_in -> right);
 
 	free(node_in);
 
-	destroy_tree(n1);
-	destroy_tree(n2);
-}
-
-void put_left (struct tree_node* node_in, int left_val){
-	if(node_in == NULL){return;}
-	node_in -> left = create_node(left_val);
-}
-
-void put_right (struct tree_node* node_in, int right_val){
-	if(node_in == NULL){return;}
-	node_in -> right = create_node(right_val);
 }
 
