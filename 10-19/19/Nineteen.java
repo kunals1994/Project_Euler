866cdb18843267370392a7e1e8a0527b87f7068b
kunals1994 <ksharma8@terpmail.umd.edu>
2014-07-30 13:27:54 -0700

    19

--- 10-19/19/Nineteen.java
+++ 10-19/19/Nineteen.java
@@ -1,18 +1,17 @@
 public class Nineteen{
 	public static void main (String[] args){
 		//Start Date
-		Date x = new Date (1, 1 , 1900);
+		Date x = new Date (1, 1 , 1901);
 
 		Date end = new Date (31, 12 ,2000);
 
-		x.incDays(6);
+		//Jump to the next Sunday; Jan 1 1901 is a Tuesday; . 
+		x.incDays(5);
 
 		int solution = 0;
 		while(x.compareTo(end) > 0){
 			if(x.getDay() == 1){
-				solution +=1;
-				System.out.println("Year: "+x.getYear() + " Month: " + x.getMonth() + " Day: " + x.getDay());
-	
+				solution +=1;	
 			}
 			x.incDays(7);
 		}
